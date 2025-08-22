058f2896b4a97249e3c8bacf71945f192c90de44
Maria-1204 <itsfer1204@gmail.com>
2025-08-22 10:50:05 -0500

    Calculator finished
    Help me Wilmer

--- calculator/CalculatorClass.py
+++ calculator/CalculatorClass.py
@@ -1,2 +1,24 @@
 class Calculator:
-    pass
+    def __init__ (self):
+        self.x = 0  
+        self.y= 0
+
+    def solicitar_numeros(self):
+            try:
+                self.x = int(input("Digite x: "))
+                self.y = int(input("Digite y: "))
+            except ValueError:
+                print("Error: Por favor ingrese números enteros.")
+                self.solicitar_numeros()
+
+    def suma(self, a, b):
+        return a + b
+    
+    def resta(self, a, b):
+        return a - b
+    
+if __name__ == "__main__":
+    g = Calculator() 
+    g.solicitar_numeros()
+    print("Suma: ", g.suma(g.x, g.y))
+    print("Resta: ", g.resta(g.x, g.y))
